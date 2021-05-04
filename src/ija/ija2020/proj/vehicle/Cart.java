e7da4f8c18df7237e95a88146dd8dbdbab72407f
Jakub Kocalka <jak.kocalka@gmail.com>
2021-05-04 17:41:58 +0200

    Cart contents

--- src/ija/ija2020/proj/vehicle/Cart.java
+++ src/ija/ija2020/proj/vehicle/Cart.java
@@ -308,4 +308,21 @@ public class Cart extends Observable implements Movable, Stockable, Drawable {
         gui.add(rect);
         return gui;
     }
+
+    /**
+     * Gets a dictionary containing info about the content of this cart
+     * @return Content of this shelf in the format <Goods type name, number of items on this shelf>
+     */
+    public Map<String, Integer> getContent(){
+        Map<String, Integer> result = new HashMap<>();
+        for (GoodsItem item : this.items){
+            String name = item.getGoodsType().getName();
+            if (result.containsKey(name)){
+                result.replace(name, result.get(name)+1);
+            }else {
+                result.putIfAbsent(name, 1);
+            }
+        }
+        return result;
+    }
 }
